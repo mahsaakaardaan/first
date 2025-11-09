074058ad85682e244d545f71cf571cab23865f9c
mahsa.kardan <mahsa.kardan@digikala.com>
2025-11-09 15:03:08 +0330

    fix login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
+import { redirect } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  //TODO: api
   const isLoggedIn = Boolean(request.cookies.get('access_token'));
 
   const protectedPaths = ['/profile', '/shipment', '/add-comment'];
@@ -10,34 +12,27 @@ export function middleware(request: NextRequest) {
     currentPath.startsWith(path)
   );
 
-  // ✅ تعیین آدرس اصلی سایت از .env یا URL فعلی
-  const baseUrl =
-    'http://46.34.163.193:3335' || request.nextUrl.origin;
-
-  // 🔒 اگر کاربر لاگین نکرده و داره مسیر محافظت‌شده رو باز می‌کنه
   if (!isLoggedIn && isProtected) {
-    const loginURL = new URL('/login', baseUrl);
-
-    // برمی‌گردونیمش به صفحه قبلی بعد از لاگین
+    const loginURL = new URL('/login', request.url);
     loginURL.searchParams.set(
       'callbackUrl',
       currentPath.includes('/add-comment')
         ? request.headers.get('referer') || '/'
         : request.url
     );
-
     return NextResponse.redirect(loginURL);
   }
 
-  // 🧭 اگر کاربر لاگین کرده ولی روی صفحه لاگینه → بفرستش به callbackUrl یا صفحه اصلی
   if (isLoggedIn && currentPath === '/login') {
     const callbackUrl =
       request.nextUrl.searchParams.get('callbackUrl');
 
-    const redirectUrl = callbackUrl
-      ? new URL(callbackUrl, baseUrl)
-      : new URL('/', baseUrl);
+      const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || request.nextUrl.origin;
 
+      
+    const redirectUrl = callbackUrl
+      ? new URL(callbackUrl,baseUrl)
+      : new URL('/', baseUrl);
     return NextResponse.redirect(redirectUrl);
   }
 
